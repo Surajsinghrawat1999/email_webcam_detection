5309737261ed5cdaf00fdc34a01d58a181e57cb4
Suraj Rawat <surajsinghrawat1999@gmail.com>
2023-07-25 20:54:57 +0530

    make  a func in send_email and when any obj is determined then send email is printed on screen

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import cv2
 import time
+from emailing import send_email
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
 first_frame = None
+status_list = []
+
 
 while True:
-
+    status = 0
     check, frame = video.read()
 
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -25,8 +28,16 @@ while True:
         if cv2.contourArea(contour) < 5000:
             continue
         x, y, w, h = cv2.boundingRect(contour)
-        cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 3)
+        rectangle = cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 3)
+        if rectangle.any():
+            status = 1
+    status_list.append(status)
+    status_list = status_list[-2:]
 
+    if status_list[0] == 1 and status_list[1] == 0:
+        send_email()
+
+    print(status_list)
     cv2.imshow("Video", frame)
     key = cv2.waitKey(1)
 
