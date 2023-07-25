eb5d232f6947bf790f61b5bdb0ca94ea2014afc0
Suraj Rawat <surajsinghrawat1999@gmail.com>
2023-07-25 20:16:08 +0530

    Open video using opencv-python module

---
+++ main.py
@@ -0,0 +1,15 @@
+import cv2
+import time
+
+video = cv2.VideoCapture(0)
+time.sleep(1)
+
+while True:
+
+    check, frame = video.read()
+    cv2.imshow("My Video", frame)
+    key = cv2.waitKey(1)
+
+    if key == ord("q"):
+        break
+video.release()
