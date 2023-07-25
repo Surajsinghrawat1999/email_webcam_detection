87dbb4f22b364fb982a5e30523f7cd8994c1851f
Suraj Rawat <surajsinghrawat1999@gmail.com>
2023-07-25 23:53:06 +0530

    send if any object is detected to gmail account

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import cv2
 import time
 from emailing import send_email
+import glob
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
 first_frame = None
 status_list = []
-
+count = 1
 
 while True:
     status = 0
@@ -31,11 +32,18 @@ while True:
         rectangle = cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 3)
         if rectangle.any():
             status = 1
+            cv2.imwrite(f"images/{count}.png", frame)
+            count = count + 1
+            all_images = glob.glob("images/*.png")
+            index = int(len(all_images) / 2)
+            image_with_object = all_images[index]
+
+
     status_list.append(status)
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email()
+        send_email(image_with_object)
 
     print(status_list)
     cv2.imshow("Video", frame)
